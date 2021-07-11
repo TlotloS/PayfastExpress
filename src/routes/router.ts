50245ce44e302a31bf9d79005c281c787fd60647
Tlotlo Nicholas <Sebeelatt7@gmail.com>
2021-07-11 21:58:07 +0200

    Added User/Auth API endpoint

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,10 @@
 import testRouter from "./testAPI";
+import userRouter from "./userAPI";
 import express  from "express";
+
 const router = express.Router();
 
+router.use("/users",userRouter);
 router.use("/test",testRouter);
 
 export default router;
